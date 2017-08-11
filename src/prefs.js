3efeae1d4ec806491ee7d04e4ad054d75cc2add0
Andy Holmes <andrew.g.r.holmes@gmail.com>
2017-08-10 18:50:41 -0700

    fix missing GLib import

--- src/prefs.js
+++ src/prefs.js
@@ -4,6 +4,7 @@ const Lang = imports.lang;
 const Gettext = imports.gettext.domain('gnome-shell-extension-mconnect');
 const _ = Gettext.gettext;
 const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
 const Gtk = imports.gi.Gtk;
 
